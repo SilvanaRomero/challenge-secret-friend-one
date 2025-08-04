0226c255026fe162a1ae5cda463d47feb3e1a2d7
Silvana <sbromero@fi.uba.ar>
2025-08-04 17:14:18 -0300

    Agrego funcion printFriend, modificación funcion friendAdd: modificación de validación y agrego alert

--- app.js
+++ app.js
@@ -8,16 +8,17 @@ function friendAdd(){
 
     // Verificar que no ingrese una cadena vacia
 
-    if ( (friend === '') || ( isNaN(friend))){
+    if ( (friend === '') || ( !isNaN(friend))){
 
         // Si ingresa una cadena vacia vuelve a pedir el nombre
 
-        document.getElementById('friend').placeholder = "Nombre incorrecto. Por favor, ingrese el nombre de un amigo: ";
+        alert("Nombre incorrecto. Por favor, ingrese el nombre de un amigo: ");
 
     } else {
 
         friendList.push(friend);
         cleanDisplay();
+        printFriends(document.getElementById('friend-list'));
 
     }
 
@@ -27,3 +28,16 @@ function cleanDisplay(){
     document.querySelector('#friend').value = ''; // Uso el querySelector por Id con #
 }
 
+// funcion para mostrar los nombres en la lista arriba del boton sorteo
+function printFriends(element){
+    /* Recibe un elemento del DOM html, lo limpia y recorre la lista de amigos, por cada amigo
+    se agrega un elemento <li> */
+
+    element.innerHTML = "";
+    for (let i = 0; i < friendList.length; i++){
+
+        let li = document.createElement("li");
+        li.textContent = friendList[i];
+        element.appendChild(li);
+    }
+}
